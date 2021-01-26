90147fdc0baff45e80ca3b12418a68c6b4619b82
Rami Hansen <rami.hansen2@gmail.com>
2021-01-26 15:03:44 -0800

    initial structure

---
+++ data-store/ds_data_store.hpp
@@ -0,0 +1,21 @@
+
+
+/**
+ * Reader
+ * Identifies a data reader
+ */
+struct Reader
+{
+};
+
+/**
+ * Data_Store
+ * Hold all recorded biometric data.
+ * Coordinate data access to other objects in other threads.
+ */
+template <typename LENGTH>
+class Data_Store
+{
+private:
+public:
+};
---
+++ data-store/ds_looping_buffer.hpp
@@ -0,0 +1,12 @@
+
+
+/**
+ * Looping_Buffer
+ * Constant buffer for reading and writing
+ */
+template <typename T>
+class Looping_Buffer
+{
+private:
+public:
+};
