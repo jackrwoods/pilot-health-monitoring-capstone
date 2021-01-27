91bafeb7c756cdb1ccb7faf1f14ff4e8f6612133
Rami Hansen <rami.hansen2@gmail.com>
2021-01-26 16:03:50 -0800

    looping buffer function prototypes

--- data-store/ds_looping_buffer.hpp
+++ data-store/ds_looping_buffer.hpp
@@ -1,13 +1,23 @@
 #pragma once
 
+#include <mutex>
+
 #include "ds_types.hpp"
 /**
  * Looping_Buffer
  * Constant buffer for reading and writing
  */
-template <typename T>
+template <typename TYPE, int LENGTH>
 class Looping_Buffer
 {
 private:
+	T buffer[LENGTH];
+	std::mutex mut;
+	uint32_t count; // count the number of received samples
 public:
+	int block_read(int from, int to, TYPE *dest);
+	int try_read(int from, int to, TYPE *dest);
+
+	int block_write(TYPE *src, size_t len);
+	int try_write(TYPE *src, size_t len);
 };
