91d9ea588fd4f67a249fb4a975fa0868f2121422
Rami Hansen <rami.hansen2@gmail.com>
2021-01-27 18:04:57 -0800

    sql databse connection works

--- data-store/ds_types.hpp
+++ data-store/ds_types.hpp
@@ -4,3 +4,13 @@
 
 using po2_sample = uint32_t;
 using optical_sample = uint32_t;
+
+struct Sample {
+    po2_sample po2;
+    optical_sample optical;
+};
+
+enum Pilot_State {
+    UNSTRESSED,
+    STRESSED
+};
---
+++ data-store/sql.cpp
@@ -0,0 +1,17 @@
+// g++ sql.cpp -lmariadb -o test.out
+#include <iostream>
+
+#include "./sql_con.hpp"
+
+// small program to demonstrate that the SQL_Connection object works as intended
+int main()
+{
+	SQL_Connection sql;
+	std::vector<Sample> ins;
+	for (int i = 0; i < 64; i++)
+		ins.push_back({static_cast<po2_sample>(i), static_cast<optical_sample>(i)});
+
+	std::cout << sql.insert_samples(ins) << std::endl;
+	std::cout << sql.insert_sample({0, 0}) << std::endl;
+	std::cout << sql.insert_state(Pilot_State::STRESSED) << std::endl;
+}
