db43643ff3f4f76dc54e482b800525a7c92739c9
Rami Hansen <rami.hansen2@gmail.com>
2021-02-16 15:27:53 -0800

    initial bluetooth connection structure

---
+++ mock-po2/bluetooth_con.hpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <cstdlib>
+#include <unistd.h>
+#include <sys/socket.h>
+
+#include <bluetooth/bluetooth.h>
+#include <bluetooth/hci.h>
+#include <bluetooth/hci_lib.h>
+
+#include <random>
+
+#define MAX_TRANSMIT_SIZE 64
+#define MIN_TRANSMIT_SIZE 32
+
+class Bluetooth_Connection
+{
+private:
+    int average{0};
+    int variance{0};
+
+    uint32_t data_buffer[MAX_TRANSMIT_SIZE];
+
+public:
+    Bluetooth_Connection();
+    Bluetooth_Connection(std::string MAC);
+    ~Bluetooth_Connection();
+
+    bool connect(std::string MAC);
+    bool close();
+
+    int adjust_po2(int i);
+};
