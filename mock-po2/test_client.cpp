320b882fc066664f31ec81226f12a607e211b2a7
Rami Hansen <rami.hansen2@gmail.com>
2021-02-18 13:28:49 -0800

    added bluetooth address in argv for client test

--- mock-po2/test_client.cpp
+++ mock-po2/test_client.cpp
@@ -3,16 +3,24 @@
 
 #include "./bt_client.hpp"
 
-int main()
+int main(int argc, char *argv[])
 {
+    if (argc != 2)
+    {
+        std::cerr << "Exactly two arguments required: ./test_client.out [blueooth address]" << std::endl;
+        return 1;
+    }
+
     std::cout << "Client Test" << std::endl;
     Bluetooth_Connection::Client c;
-    c.open_con("");
+    
+    std::cout << "Opening connection: " << c.open_con(std::string(argv[1])) << std::endl;
 
     std::thread client(&Bluetooth_Connection::Client::run, &c);
 
     std::string in;
-    while(in != "q") {
+    while (in != "q")
+    {
         std::getline(std::cin, in);
         Bluetooth_Connection::Packet p(in.length(), in.c_str());
         c.push(p);
