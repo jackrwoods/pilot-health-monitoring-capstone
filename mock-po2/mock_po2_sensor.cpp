67c51abc7f56374f15789976a2d0580b9d75f207
Rami Hansen <rami.hansen2@gmail.com>
2021-02-21 16:13:37 -0800

    added mock po2 sensor tests and updated Makefile

---
+++ mock-po2/mock_po2_sensor.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+#include "./bluetooth/bluetooth_con.hpp"
+
+int main(int argc, char *argv[])
+{
+    std::cout << "Mock PO2 Sensor Send test" << std::endl;
+    return 0;
+}
---
+++ mock-po2/mock_po2_sensor_client.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+#include "./bluetooth/bluetooth_con.hpp"
+
+int main(int argc, char *argv[])
+{
+    std::cout << "Mock PO2 Sensor Recieve test ('hcitool dev' in terminal returns the bluetooth address of this device)" << std::endl;
+    return 0;
+}
