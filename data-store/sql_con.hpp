22b5e7abf352faf23b3bcc5e9bc87957682a9c4c
Rami Hansen <rami.hansen2@gmail.com>
2021-02-23 11:11:21 -0800

    uses sqlite3 now, added some more timing tests

--- data-store/sql_con.hpp
+++ data-store/sql_con.hpp
@@ -1,34 +1,24 @@
-#include <mariadb/mysql.h> // -lmariadb
+#include <sqlite3.h>
 #include <iostream>
 #include <string>
 #include <vector>
 
 #include "ds_types.hpp"
 
-// #defines for common parts of SQL queries
-#define SQL_SERVER "localhost"
-#define SQL_USER "test"
-#define SQL_PASSWORD "password"
-#define SQL_DATABASE "capstone_test"
-
 // biometric samples table
 #define SQL_SAMPLE_TABLE "samples"
 #define SQL_SAMPLE_SCHEMA "( \
-						sample_id INT NOT NULL AUTO_INCREMENT, \
 						sample_time BIGINT, \
 						optical INT, \
-						po2 INT, \
-						PRIMARY KEY (sample_id) \
+						po2 INT \
 					);"
 #define SQL_SAMPLE_COLUMN_LIST "(sample_time, optical, po2)"
 
 // pilot state table
 #define SQL_STATE_TABLE "pilot_state"
 #define SQL_STATE_SCHEMA "( \
-						state_id int NOT NULL AUTO_INCREMENT, \
 						timestamp DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP, \
-						state INT, \
-						PRIMARY KEY (state_id) \
+						state INT \
 					);"
 #define SQL_STATE_COLUMN_LIST "(state)"
 
@@ -39,7 +29,8 @@
 class SQL_Connection
 {
 private:
-	MYSQL sql;
+	// MYSQL sql;
+	sqlite3 *database;
 
 	int query_execute(const char *c);
 
@@ -51,6 +42,7 @@ public:
 	int insert_sample(const Sample s);
 
 	int insert_state(const Pilot_State s);
+	int insert_states(const std::vector<Pilot_State> &v);
 };
 
 /**
@@ -60,51 +52,50 @@ public:
  */
 int SQL_Connection::query_execute(const char *c)
 {
-	if (mysql_query(&sql, c) != 0)
-	{
-		fprintf(stderr, "Error occurred executing: %s\n", c);
-		return 1;
-	}
+	char *error;
+	int exit = sqlite3_exec(database, c, NULL, 0, &error);
 
+	if (exit != SQLITE_OK)
+	{
+		std::cerr << "SQL Error: " << error << std::endl;
+		sqlite3_free(error);
+	}
 	return 0;
 }
 
 SQL_Connection::SQL_Connection()
 {
 	// create sql connection - halt if error
-	mysql_init(&sql);
-	if (!mysql_real_connect(&sql, SQL_SERVER, SQL_USER, SQL_PASSWORD, SQL_DATABASE, 0, nullptr, 0))
+	if (sqlite3_open("samples.db", &database))
 	{
-		fprintf(stderr, "Failed to connect to database: Error: %s\n", mysql_error(&sql));
+		std::cerr << "An error occurred connecting to the database" << sqlite3_errmsg(database) << std::endl;
 		exit(1);
 	}
 
 	// Drop sample table if exists - halt if error
-	const char *cmd_drop_sample_table = "DROP TABLE IF EXISTS " SQL_DATABASE "." SQL_SAMPLE_TABLE ";";
+	const char *cmd_drop_sample_table = "DROP TABLE IF EXISTS " SQL_SAMPLE_TABLE ";";
 	if (query_execute(cmd_drop_sample_table))
 		exit(1);
 
 	// Create new table for storing samples - halt if error
-	const char *cmd_create_sample_table = "CREATE TABLE " SQL_DATABASE "." SQL_SAMPLE_TABLE " " SQL_SAMPLE_SCHEMA;
+	const char *cmd_create_sample_table = "CREATE TABLE " SQL_SAMPLE_TABLE " " SQL_SAMPLE_SCHEMA;
 	if (query_execute(cmd_create_sample_table))
 		exit(1);
 
-
-
 	// Drop state table if exists - halt if error
-	const char *cmd_drop_state_table = "DROP TABLE IF EXISTS " SQL_DATABASE "." SQL_STATE_TABLE ";";
+	const char *cmd_drop_state_table = "DROP TABLE IF EXISTS " SQL_STATE_TABLE ";";
 	if (query_execute(cmd_drop_state_table))
 		exit(1);
 
 	// Create new table for storing pilot state - halt if error
-	const char *cmd_create_state_table = "CREATE TABLE " SQL_DATABASE "." SQL_STATE_TABLE " " SQL_STATE_SCHEMA;
+	const char *cmd_create_state_table = "CREATE TABLE " SQL_STATE_TABLE " " SQL_STATE_SCHEMA;
 	if (query_execute(cmd_create_state_table))
 		exit(1);
 }
 
 SQL_Connection::~SQL_Connection()
 {
-	mysql_close(&sql);
+	sqlite3_close(database);
 }
 
 /**
@@ -116,7 +107,7 @@ int SQL_Connection::insert_samples(const std::vector<Sample> &v)
 {
 	// construct sql statement
 	// loop through each Sample in v and insert into statement
-	std::string cmd_insert = "INSERT INTO " SQL_DATABASE "." SQL_SAMPLE_TABLE " " SQL_SAMPLE_COLUMN_LIST " VALUES ";
+	std::string cmd_insert = "INSERT INTO " SQL_SAMPLE_TABLE " " SQL_SAMPLE_COLUMN_LIST " VALUES ";
 	for (auto vi = v.begin(); vi != v.end(); vi++)
 	{
 		cmd_insert += "(" + std::to_string(vi->time_stamp.time_since_epoch().count()) + ',' + std::to_string(vi->optical) + ',' + std::to_string(vi->po2) + ")";
@@ -136,7 +127,7 @@ int SQL_Connection::insert_samples(const std::vector<Sample> &v)
  */
 int SQL_Connection::insert_sample(const Sample s)
 {
-	std::string cmd_insert = "INSERT INTO " SQL_DATABASE "." SQL_SAMPLE_TABLE " " SQL_SAMPLE_COLUMN_LIST " VALUES (" + std::to_string(s.time_stamp.time_since_epoch().count()) + ','  + std::to_string(s.po2) + ',' + std::to_string(s.optical) + ");";
+	std::string cmd_insert = "INSERT INTO " SQL_SAMPLE_TABLE " " SQL_SAMPLE_COLUMN_LIST " VALUES (" + std::to_string(s.time_stamp.time_since_epoch().count()) + ',' + std::to_string(s.po2) + ',' + std::to_string(s.optical) + ");";
 	return query_execute(cmd_insert.c_str());
 }
 
@@ -147,6 +138,25 @@ int SQL_Connection::insert_sample(const Sample s)
  */
 int SQL_Connection::insert_state(const Pilot_State s)
 {
-	std::string cmd_insert = "INSERT INTO " SQL_DATABASE "." SQL_STATE_TABLE " " SQL_STATE_COLUMN_LIST " VALUES(" + std::to_string(static_cast<int>(s)) + ");";
+	std::string cmd_insert = "INSERT INTO " SQL_STATE_TABLE " " SQL_STATE_COLUMN_LIST " VALUES(" + std::to_string(static_cast<int>(s)) + ");";
+	return query_execute(cmd_insert.c_str());
+}
+
+/**
+ * insert_states: Insert multiple pilot states into the database.
+ * @param v std::vector of the last n recorded pilot states
+ * @returns zero on success, nonzero on error
+ */
+int SQL_Connection::insert_states(const std::vector<Pilot_State> &v)
+{
+	std::string cmd_insert = "INSERT INTO " SQL_STATE_TABLE " " SQL_STATE_COLUMN_LIST " VALUES ";
+	for (auto vi = v.begin(); vi != v.end(); vi++)
+	{
+		cmd_insert += "(" + std::to_string(static_cast<int>(*vi)) + ")";
+
+		if (vi + 1 != v.end())
+			cmd_insert += ',';
+	}
+	cmd_insert += ';';
 	return query_execute(cmd_insert.c_str());
 }
