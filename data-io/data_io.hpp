f20f7e0e1b939406f3d8b0a9e9739522845c6c25
Rami Hansen <rami.hansen2@gmail.com>
2021-03-08 14:03:27 -0800

    doxygen comments

--- data-io/data_io.hpp
+++ data-io/data_io.hpp
@@ -4,6 +4,9 @@
 #include "./bluetooth/bt_server.hpp"
 #include "./io_types.hpp"
 
+/**
+ * Data_IO: Receive Bluetooth data and write to datastore
+ */
 template <typename SAMPLE_TYPE>
 class Data_IO
 {
@@ -26,26 +29,47 @@ public:
     void send_pilot_state(Pilot_State state);
 };
 
+/**
+ * run: Begin the recieve loop.
+ */
 template <class SAMPLE_TYPE>
 void Data_IO<SAMPLE_TYPE>::run()
 {
 }
 
+/**
+ * send_pilot_state: Transmit pilot state over Bluetooth connection
+ * @param state: Pilot state to send.
+ */
 template <class SAMPLE_TYPE>
 void Data_IO<SAMPLE_TYPE>::quit()
 {
 }
 
+/**
+ * register_callback: Register a function to use whenever new data is available.
+ * @param fn: Callback function
+ * @returns: True if successful, false otherwise
+ */
 template <class SAMPLE_TYPE>
 bool Data_IO<SAMPLE_TYPE>::register_callback(std::function<int(SAMPLE_TYPE *, size_t)> fn)
 {
 }
 
+/**
+ * connect: Initialize a Bluetooth connection.
+ * @param bluetooth_address: Bluetooth address to connect to.
+ * @returns: True if successful, false otherwise
+ */
 template <class SAMPLE_TYPE>
 bool Data_IO<SAMPLE_TYPE>::connect(std::string bluetooth_address)
 {
 }
 
+/**
+ * send_pilot_state: Transmit pilot state over Bluetooth connection
+ * @param state: Pilot state to send.
+ */
 template <class SAMPLE_TYPE>
 void Data_IO<SAMPLE_TYPE>::send_pilot_state(Pilot_State state)
 {
