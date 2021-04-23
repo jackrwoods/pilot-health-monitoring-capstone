f8462a350af93dfd4a9c8e41af79b5c61f087847
Rami Hansen <rami.hansen2@gmail.com>
2021-04-22 17:08:07 -0700

    redoing sendDataToAllClients fixes that got lost somehow

--- data-server/src/socketServer.cpp
+++ data-server/src/socketServer.cpp
@@ -16,32 +16,34 @@ WsServer server;
 
 // Simple datasource callback.
 // This produces a json string and sends it to all websocket clients.
-void sendDataToAllClients(struct Sample* data) {
-	if (data->sourceType == MAX30100) {
-		srand(time(NULL));
+void sendDataToAllClients(struct Sample *data)
+{
+	srand(time(NULL));
 
-		std::string json = "{\"timestamp\": ";
-		json += std::to_string(data->timestamp);
-		json += ",\"temperature\": ";
-		json += std::to_string(data->temperature.value);
-		json += ", \"HR\": ";
-		json += std::to_string(5.0 * sin(rand() % 100) + 80);
-		json +=	", \"SpO2\": ";
-		json += std::to_string(5.0 * sin(rand() % 10) + 95);
-		json += ", \"sentTimestamp\": ";
-		json += std::to_string(std::chrono::time_point_cast<std::chrono::milliseconds>(std::chrono::high_resolution_clock::now()).time_since_epoch().count());
-		json += "}";
+	std::string json = "{\"timestamp\": ";
+	json += std::to_string(data->timestamp);
+	json += ",\"temperature\": ";
+	json += std::to_string(data->temperature.value);
+	json += ", \"HR\": ";
+	json += std::to_string(data->bpm.value);
+	json += ", \"SpO2\": ";
+	json += std::to_string(data->spo2.value);
+	json += ", \"sentTimestamp\": ";
+	json += std::to_string(std::chrono::time_point_cast<std::chrono::milliseconds>(std::chrono::high_resolution_clock::now()).time_since_epoch().count());
+	json += "}";
 
-		// Send the latest datapoint to all clients
-		for (auto &c : server.get_connections()) {
-			if (c->path == "/data") {
-				c->send(json);
-			}
+	// Send the latest datapoint to all clients
+	for (auto &c : server.get_connections())
+	{
+		if (c->path == "/data")
+		{
+			c->send(json);
 		}
 	}
 }
 
-void startServer(Datasource* datasource) {
+void startServer(Datasource *datasource)
+{
 	// Start the websocket server on port 8080 using 1 thread
 	server.config.port = 8080;
 
@@ -60,7 +62,7 @@ void startServer(Datasource* datasource) {
 	};
 	ping.on_error = [](std::shared_ptr<WsServer::Connection> connection, const SimpleWeb::error_code &ec) {
 		std::cout << "Server: Error in connection " << connection.get() << ". "
-			<< "Error: " << ec << ", error message: " << ec.message() << "\n";
+				  << "Error: " << ec << ", error message: " << ec.message() << "\n";
 	};
 	ping.on_message = [](std::shared_ptr<WsServer::Connection> connection, std::shared_ptr<WsServer::InMessage> msg) {
 		connection->send(":)");
@@ -83,7 +85,7 @@ void startServer(Datasource* datasource) {
 	// See http://www.boost.org/doc/libs/1_55_0/doc/html/boost_asio/reference.html, Error Codes for error code meanings
 	data.on_error = [](std::shared_ptr<WsServer::Connection> connection, const SimpleWeb::error_code &ec) {
 		std::cout << "Server: Error in connection " << connection.get() << ". "
-			<< "Error: " << ec << ", error message: " << ec.message() << "\n";
+				  << "Error: " << ec << ", error message: " << ec.message() << "\n";
 	};
 
 	std::promise<unsigned short> server_port;
@@ -95,10 +97,10 @@ void startServer(Datasource* datasource) {
 	});
 
 	std::cout << "Server listening on " << server_port.get_future().get()
-		<< "\n";
+			  << "\n";
 
 	// Set up the datasource
-	std::function<void(struct Sample*)> callback = sendDataToAllClients;
+	std::function<void(struct Sample *)> callback = sendDataToAllClients;
 	datasource->registerCallback(callback);
 
 	server_thread.join();
